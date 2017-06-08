60ece01a670ac336ba4f3088548f096bb088c5c4
Student <xabhay17@gmail.com>
2017-06-07 22:16:19 -1200

    aa

---
+++ 1543endssem/src/EVM/BallotUnit.java
@@ -0,0 +1,14 @@
+package EVM;
+
+public class BallotUnit 
+{
+boolean button=true;
+String candidate;	
+
+//check if vote is casted
+public void setButton(boolean button)
+{
+	this.button=button;
+}
+
+}
